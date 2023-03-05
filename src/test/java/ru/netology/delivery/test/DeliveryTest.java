d60c504ace205d4ba71185c8a7043bea99c0846e
Nadezhda Komarova <78150221+KomarovaN@users.noreply.github.com>
2023-03-05 23:15:16 +0300

    Update DeliveryTest.java

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -17,11 +17,6 @@ import static com.codeborne.selenide.Selenide.open;
 
 class DeliveryTest {
 
-    @BeforeEach
-    void setup() {
-        open("http://localhost:9999");
-    }
-
     @Test
     void shouldSubmitRequest() {
         DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
